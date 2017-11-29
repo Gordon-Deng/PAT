2bfdf6b19b0d9c0acded7742194c65408173c647
Gordon <gordondeng@foxmail.com>
2017-11-29 22:00:31 +0800

    T4
    正整数A的“DA（为1位整数）部分”定义为由A中所有DA组成的新整数PA。例如：给定A = 3862767，DA = 6，则A的“6部分”PA是66，因为A中有2个6。
    
     现给定A、DA、B、DB，请编写程序计算PA + PB。

---
+++ T6.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class T6 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String []in = new String[4];
+        in = bf.readLine().split(" ");
+        String S1 = in[0];
+        String N1 = in[1];
+        String S2 = in[2];
+        String N2 = in[3];
+
+        int count1= 0,count2 = 0;
+        for (int i = 0; i < S1.length(); i++) {
+            if (S1.charAt(i) == N1.charAt(0))
+                count1++;
+        }
+
+        int P1 = 0;
+        for (int i = 0; i < count1; i++) {
+            P1 += Integer.parseInt(N1)*Math.pow(10,i);
+        }
+
+        for (int i = 0; i < S2.length(); i++) {
+            if (S2.charAt(i) == N2.charAt(0))
+                count2++;
+        }
+
+        int P2 = 0;
+        for (int i = 0; i < count2; i++) {
+            P2 += Integer.parseInt(N2)*Math.pow(10,i);
+        }
+
+        System.out.println(P1+P2);
+    }
+}
