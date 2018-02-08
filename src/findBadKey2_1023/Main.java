4a3d03c2719e5438538359d666a6e16b79a59196
Gordon <gordondeng@foxmail.com>
2018-02-09 03:41:36 +0800

    add 1023

--- src/findBadKey2_1023/Main.java
+++ src/findBadKey2_1023/Main.java
@@ -3,10 +3,7 @@ package findBadKey2_1023;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,10 +11,11 @@ public class Main {
 
         String badKey = bf.readLine();
         String inputData = bf.readLine();
+        int deleteNum = 0;
         char[] inputData_Char = inputData.toCharArray();
         bf.close();
 
-        Set<Integer> indexOfKey = new HashSet<Integer>();
+        TreeSet<Integer> indexOfKey = new TreeSet<Integer>();
         boolean flag = false;
 
         for (int i = 0; i < badKey.length(); i++) {
@@ -28,7 +26,6 @@ public class Main {
         }
 
         if (!flag){
-            System.out.println("++++++");
             String inputData_Upper = inputData.toUpperCase();
 
             for (int i = 0; i < badKey.length(); i++) {
@@ -41,26 +38,24 @@ public class Main {
         }
 
         else {
-            System.out.println("------");
             String badKey_Lower = badKey.toLowerCase();
-
-            for (int i = 0; i < badKey.length(); i++) {
-                for (int j = 0; j < inputData.length(); j++) {
-                    if (badKey_Lower.charAt(i) == inputData.charAt(j) || Character.isUpperCase(inputData.charAt(j))){
-                        System.out.println(inputData.charAt(j));
-                        indexOfKey.add(j);
+            loop2:
+                for (int i = 0; i < inputData.length(); i++) {
+                    for (int j = 0; j < badKey.length(); j++) {
+                        if (badKey_Lower.charAt(j) == inputData.charAt(i) || Character.isUpperCase(inputData.charAt(i))){
+                            indexOfKey.add(i);
+                            continue loop2;
+                        }
                     }
                 }
-            }
         }
 
         StringBuilder sb = new StringBuilder(inputData);
-        //Iterator<Integer> iterator = indexOfKey.iterator();
-        //while (iterator.hasNext()){
-        //    sb.deleteCharAt(iterator.next());
-        //}
-
-        indexOfKey.
+        Iterator<Integer> iterator = indexOfKey.iterator();
+        while (iterator.hasNext()){
+            sb.deleteCharAt(iterator.next()-deleteNum);
+            deleteNum ++;
+        }
         System.out.println(sb.toString());
     }
 }
