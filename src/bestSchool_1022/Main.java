7ec9d0360bf7f6040f545cadca3b9dbcdad3b288
Gordon <gordondeng@foxmail.com>
2018-02-08 23:37:53 +0800

    add 1022

---
+++ src/bestSchool_1022/Main.java
@@ -0,0 +1,14 @@
+package bestSchool_1022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int participantNum = Integer.parseInt(bf.readLine());
+        
+    }
+}
